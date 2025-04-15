2fd45aa82c9f17dcf8b3e119a7519a8fce39d8c3
Rick <rickewald@gmx.de>
2025-04-15 22:53:03 +0200

    changed some shit

--- Panda_Train.py
+++ Panda_Train.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_excel('C:\\Users\\RickE\\Downloads\\movies.xls')
+genres_list = df['Genres'].str.split('|').explode()
 
-high_rated_movies = df.loc[df['Year'] > 1000, ['Title', 'IMDB Score']]
-print(df.columns)                 
-print(high_rated_movies)
+# Count the occurrences of each genre
+genre_counts = genres_list.value_counts()
+
+# Get the top 5 and bottom 5 genres
+top_5_genres = genre_counts.head(5)
+bottom_5_genres = genre_counts.tail(5)
+
+print("Top 5 most common genres:")
+print(top_5_genres)
+
+print("\nFlop 5 least common genres:")
+print(bottom_5_genres)
